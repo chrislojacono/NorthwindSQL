df97251e0dbace33c550b908d4e7f334d1563cec
Chris LoJacono <clojacono92@gmail.com>
2021-03-06 10:09:03 -0600

    sum

--- NorthwindQuestions.sql
+++ NorthwindQuestions.sql
@@ -1,4 +1,4 @@
-select o.* ,(od.Quantity * od.UnitPrice) as OrderPrice
+select o.* ,sum(od.Quantity * od.UnitPrice) as OrderPrice
 from Orders o
 	Join [Order Details] OD
 	on od.OrderID = o.OrderID
